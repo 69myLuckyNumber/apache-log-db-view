9511032888fe679e7dfd27628f5a1844a4a6d532
Yurnero <muravyoov@gmail.com>
2018-03-30 16:42:45 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ApacheDBView
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dbGridView = new System.Windows.Forms.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.dbGridView)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dbGridView
+			// 
+			this.dbGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dbGridView.Location = new System.Drawing.Point(-3, 0);
+			this.dbGridView.Name = "dbGridView";
+			this.dbGridView.Size = new System.Drawing.Size(895, 563);
+			this.dbGridView.TabIndex = 0;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(918, 562);
+			this.Controls.Add(this.dbGridView);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dbGridView)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dbGridView;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApacheDBView
+{
+	public partial class Form1 : Form
+	{
+		private readonly string connectionString;
+
+		public Form1()
+		{
+			connectionString = "Server=.\\SQLEXPRESS;Database=apache-db;Trusted_Connection=True;";
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+			using (var connection = new SqlConnection(connectionString))
+			{
+				SqlDataAdapter sqlAdapter = new SqlDataAdapter();
+
+				using (var sqlCommand = new SqlCommand(@"SELECT CAST(h.IPAddressBytes as varchar(MAX)) HostIp, 
+														h.HostName HostName,
+														h.OrgName Org, 
+														r.RequestType [Type], 
+														r.ResponseStatusCode Code, 
+														r.DateTimeRequested [DateTime], 
+														f.FilePath [Path], 
+														f.FileName [FileName], f.FileSize Size  
+
+														FROM dbo.Hosts h
+														JOIN dbo.Requests r
+															ON r.RequestorIPAddress = h.IPAddressBytes
+														JOIN dbo.Files f
+															ON f.RequestId = r.RequestId")) 
+				{
+					sqlCommand.Connection = connection;
+					try
+					{
+						sqlAdapter.SelectCommand = sqlCommand;
+						DataTable dbSet = new DataTable();
+						sqlAdapter.Fill(dbSet);
+						BindingSource bindingSource = new BindingSource();
+
+						bindingSource.DataSource = dbSet;
+
+						dbGridView.DataSource = bindingSource;
+						sqlAdapter.Update(dbSet);
+					}
+					catch (Exception ex)
+					{
+						MessageBox.Show(ex.Message);
+					}
+
+				}
+			}			
+		}
+	}
+}
